40813ff78881fa016e43560eb629cad13bd50310
jmin4078 <wpalsrl8588@gmail.com>
2026-07-28 15:10:37 +0900

    Initial commit

---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<html>
+<head>
+  <title>당신의 사주를 봐드립니다</title>
+</head>
+<body>
+<h1>AI가 봐주는 사주</h1>
+</body>
+</html>
---
+++ src/test/java/org/example/saju/SajuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.saju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SajuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
